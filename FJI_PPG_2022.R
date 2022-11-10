522fc3a66739ab1c7b85a323181bf5b95545d549
luisrua <luisruarodriguez@gmail.com>
2022-11-10 13:29:40 +0700

    NRU correction notes proj Adding FJI updated script

--- FJI_PPG_2022.R
+++ FJI_PPG_2022.R
@@ -1,4 +1,4 @@
-# POPULATION GRID NAURU - NRU - 2022 UPDATE
+# POPULATION GRID Fiji - FJI - 2022 UPDATE
 
 # Use of census data and country projections to generate 33m and 100m resolution population grids
 ## Luis de la Rua - luisr@spc.int - October 2022
@@ -21,23 +21,23 @@ wd <- "C:/git/spc/popgrid_2022"
 setwd(wd)
 
 ## Country code to name output data and other files
-country <- 'NRU'
+country <- 'FJI'
 # Data directory
 dd <- paste0("C:/Users/luisr/SPC/SDD GIS - Documents/Pacific PopGrid/UPDATE_2022/",country)
 
 # Define Population Growth Rates parameters
-census_year <- 2021
+census_year <- 2017
 current_year <- 2022
-pop_2021 <- 11690  # calculated from latest census data
-prjpop_2022 <-  11928  # From SDD .STAT population projections
+pop_2017 <- 884887  # calculated from latest census data
+prjpop_2022 <-  901603   # From SDD .STAT population projections
 
 # check if there is pgr defined in census report
-popGR <- (prjpop_2022 - pop_2021)/((current_year - census_year)*pop_2021)
+popGR <- (prjpop_2022 - pop_2017)/((current_year - census_year)*pop_2017)
 popGR
 
 # POULATION SPATIAL DISTRIBUTION INPUT
 # HH locations and population from 2020 PHC
-pts <- st_read(paste0(dd,"/layers/NRU_hhlocPHC_2021.gpkg"))
+pts <- st_read(paste0(dd,"/layers/FB_2007_combined_ahs2017_3832.gpkg"))
 
 # Bring Zone layer to avoid points excluded
 zone <- st_read(paste0(dd,"/layers/zone.gpkg"))
@@ -46,18 +46,18 @@ zone <- st_read(paste0(dd,"/layers/zone.gpkg"))
 
 rast100m <- raster()
 extent(rast100m) <- extent(zone)
-res(rast100m) <- 0.001
+res(rast100m) <- 100 # Attention metric coordinate system
 rast100m
 
-# set 4326 projection
-crs(rast100m) <- CRS("+init=epsg:4326")
+# set 3832 projection
+crs(rast100m) <- CRS("+init=epsg:3832")
 rast100m
 
 # RASTERIZE Check that pop field is integer
-rastpop2021_100m <- rasterize(pts,rast100m,'pop',fun=sum)
-rastpop2021_100m
-totpop2021_count <- cellStats(rastpop2021_100m, 'sum')
-totpop2021_count
+rastpop2017_100m <- rasterize(pts,rast100m,'ahs',fun=sum)
+rastpop2017_100m
+totpop2017_count <- cellStats(rastpop2017_100m, 'sum')
+totpop2017_count
 
 # optional draw histogram ignoring NA values
 # hist(na.omit(getValues(rastpop2020_100m)))
@@ -65,12 +65,12 @@ totpop2021_count
 # writeRaster(rastpop2020_100m ,'raster/KIR_pop202.tif', overwrite=TRUE)
 
 # projecting population data up to current year
-pop_dif <- totpop2021_count*popGR * (as.numeric(current_year)- as.numeric(census_year))
+pop_dif <- totpop2017_count*popGR * (as.numeric(current_year)- as.numeric(census_year))
 pop_dif
 
 # project population on GPS location dataset
-pts2021 <- pts
-pts2021
+pts2017 <- pts
+pts2017
 
 ## ROUND PRESERVE SUM projecting population over the point layer as source
 # define formula for the round preserving sum
@@ -84,32 +84,33 @@ round_preserve_sum <- function(x, digits = 0) {
 }
 
 # create field with total population projected
-pts2021$totpop2022 <- (pts2021$pop + (pts2021$pop * popGR * (as.numeric(current_year)- as.numeric(census_year))))
-pts2021
+pts2017$pop <- pts2017$ahs
+pts2017$totpop2022 <- (pts2017$pop + (pts2017$pop * popGR * (as.numeric(current_year)- as.numeric(census_year))))
+pts2017
 
-pts_totpop2022 <- sum(pts2021$totpop2022,na.rm = T)
-pts_totpop2021 <- sum(pts2021$pop,na.rm = T)
+pts_totpop2022 <- sum(pts2017$totpop2022,na.rm = T)
+pts_totpop2017 <- sum(pts2017$pop,na.rm = T)
 
-pop_evolution <- pts_totpop2022 - pts_totpop2021
+pop_evolution <- pts_totpop2022 - pts_totpop2017
 pop_evolution
 
 # round and check to see if it works
-round_preserve_sum(pts2021$totpop2022)
+round_preserve_sum(pts2017$totpop2022)
 
-pts_totpop2022_rps<- sum(round_preserve_sum(pts2021$totpop2022))
+pts_totpop2022_rps<- sum(round_preserve_sum(pts2017$totpop2022))
 pts_totpop2022_rps
 dif <- pts_totpop2022_rps - pts_totpop2022
 dif
 
 # create the rounded field and add it into the att table
-pts2021$totpop2022rps <- round_preserve_sum(pts2021$totpop2022)
-pts2021
+pts2017$totpop2022rps <- round_preserve_sum(pts2017$totpop2022)
+pts2017
 
-sum(pts2021$totpop2022rps)
+sum(pts2017$totpop2022rps)
 # st_write(pts2020,"layers/pts2020.shp",delete_layer=TRUE)
 
-rastpop2022rps_100m<- rasterize(pts2021,rast100m,'totpop2022rps',fun=sum)
+rastpop2022rps_100m<- rasterize(pts2017,rast100m,'totpop2022rps',fun=sum)
 rastpop2022rps_100m
 cellStats(rastpop2022rps_100m, 'sum')
 
-writeRaster(rastpop2022rps_100m ,paste0(dd,"/raster/NRU_pop2022.tif"), overwrite=TRUE)
+writeRaster(rastpop2022rps_100m ,paste0(dd,"/raster/",country,"_pop2022.tif"), overwrite=TRUE)
