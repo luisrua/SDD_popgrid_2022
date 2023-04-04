99d86ecfc1fbe3424818dfa1c3ddc3ae6da97151
luisrua <luisruarodriguez@gmail.com>
2023-04-04 12:21:47 +0700

    project population, rasterie and check

--- FJI_PPG_2023.R
+++ FJI_PPG_2023.R
@@ -37,13 +37,22 @@ prjpop_2022 <-  901603   # From SDD .STAT population projections - We need to up
 popGR <- (prjpop_2022 - pop_2017)/((current_year - census_year)*pop_2017)
 popGR
 
+## Define ROUND PRESERVE SUM function
+round_preserve_sum <- function(x, digits = 0) {
+  up <- 10 ^ digits
+  x <- x * up
+  y <- floor(x)
+  indices <- tail(order(x-y), round(sum(x)) - sum(y))
+  y[indices] <- y[indices] + 1
+  y / up
+}
 
 # LOADING INPUT LAYERS
 ## hh locations from census
 hhloc <- vect(paste0(dd,"/Population Grid Data/Fiji_HH_2017.shp"))
 crs(hhloc)
 
-## We are making all the geospatial analysis using Fiji Map Grid projection EPSG 3460, grabbing it from the first layer loaded
+## We are making all the spatial analysis using Fiji Map Grid projection EPSG 3460, grabbing it from the first layer loaded
 fji_crs <- crs(hhloc)
 
 ## EA boundaries layer including census population and hh counts
@@ -67,7 +76,7 @@ hhloc_merged$val <- 1 #this is useful later to be able to count points in polygo
 
 ## Count number of hhlocations within each EA 
 
-ea_simpl<-ea[,"ea2017"] # simplify dataset
+ea_simpl<-ea[,"ea2017"] # before we simplify dataset
 # ea_simp$count <- lengths(terra::intersect(ea_simpl,hhloc_merged)) ### this does the points in polygon in one step but super slow for 170K points
 i <- intersect (ea_simpl,hhloc_merged) # run intersection
 isimp <- i[,"ea2017"] # simplify result dataframe
@@ -90,6 +99,27 @@ head(ea_ahs)
 hhloc_ahs <- intersect(i,ea_ahs)
 head(hhloc_ahs)
 hhloc_ahs <- hhloc_ahs[,c("ea2017","ahs")] # Clean unused fields from dataset
-sum(hhloc_ahs$ahs)
+sum(hhloc_ahs$ahs) # check everything is adding ok
 
-## Assign that AHS
+## Project the population for each of the hh locations
+hhloc_ahs$pop2022 <- (hhloc_ahs$ahs + (hhloc_ahs$ahs * popGR * (as.numeric(current_year)- as.numeric(census_year))))
+totpop2022 <- sum(hhloc_ahs$pop2022)
+
+## Round to get integers
+hhloc_ahs$pop2022rps <- round_preserve_sum(hhloc_ahs$pop2022)
+head(hhloc_ahs)
+totpop2022rps <- sum(hhloc_ahs$pop2022rps)
+
+## Convert the Point layer into a 100x100m raster.
+## Create a blank raster first
+ras <- rast()
+crs(ras) <- fji_crs # CRS
+ext(ras) <- ext(ea)  # set extent the same as the EA fwork
+res(ras) <- 100 # Resolution
+
+## Rasterize the hh locations dataset using the projected population
+rastpopop2022 <- terra::rasterize(hhloc_ahs,ras,'pop2022rps',fun=sum)
+
+totpop2022rps - (global(rastpopop2022, fun='sum',na.rm=T)) # checking that raster includes same population as the original, if it 0 we are good
+
+
